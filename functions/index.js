7db2a38820ac78175987c9c7f167926828fdfafe
Taiga Miura <taigamiura22@gmail.com>
2024-07-15 15:17:42 +0900

    del

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,7 @@
-const { onRequest } = require('firebase-functions/v2/https');
-const LogHandler = require('./logHandler');
+const { onRequest } = require('firebase-functions/v2/https')
 
 exports.helloWorld = onRequest({
   region: 'asia-northeast1' // 東京リージョンを指定
-}, async (request, response) => {
-
-  const logHandler = new LogHandler(request);
-  let statusCode = 200;
-  let message;
-
-  try {
-    const type = request.query.type;
-
-    switch (type) {
-      case 'warn':
-        statusCode = 400;
-        break;
-      case 'error':
-        statusCode = 500;
-        break;
-    }
-    if (400 <= statusCode && statusCode <= 500) {
-      throw Error('Internal Server Error');
-    }
-    message = 'Request handled successfully';
-  } catch (error) {
-    message = error.message;
-  } finally {
-    logHandler.log(statusCode, message);
-    response.status(statusCode).send(message);
-  }
-
-});
+}, async response => {
+  response.status(200).send('hello')
+})
